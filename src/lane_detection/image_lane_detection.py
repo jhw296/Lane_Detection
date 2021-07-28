75df02706273f4cc25031da7f7fb05a33f9f1620
johyewon <johyewon29@itgit.cu.ac.kr>
2021-07-28 13:19:07 +0900

    Completed_Creating a Single Left and Right Lane Line

--- src/lane_detection/image_lane_detection.py
+++ src/lane_detection/image_lane_detection.py
@@ -41,19 +41,27 @@ plt.imshow(cropped_image)
 
 lines = cv2.HoughLinesP (cropped_image, rho = 6, theta = np.pi/60, threshold = 160, lines = np.array([]), minLineLength = 40, maxLineGap = 25 )
 
+print(len(lines))
+print(lines)
+
 line_image = draw_lines(image, lines)
 plt.figure()
 plt.imshow(line_image)
-"""
+
+
 left_line_x = []
 left_line_y = []
 right_line_x = []
 right_line_y = []
 
+min_y = int(image.shape[0]*(3/5))
+max_y = int(image.shape[0])
 
 for line in lines:
 	for x1, y1, x2, y2 in line:
-		slope = (y2-y1)/(x2-x1)
+		print(x1, y1, x2, y2)
+		slope = float((y2 - y1)) / float((x2 - x1))
+		print(slope)
 		if math.fabs(slope) < 0.5:
 			continue
 		if slope <= 0:
@@ -63,8 +71,8 @@ for line in lines:
 			right_line_x.extend([x1, x2])
 			right_line_y.extend([y1, y2])
 
-min_y = image.shape[0]*(3/5)
-max_y = image.shape[0]
+print(line)
+print(left_line_x, left_line_y, right_line_x, right_line_y)
 
 poly_left = np.poly1d(np.polyfit(
 	left_line_y,
@@ -78,23 +86,66 @@ left_x_end = int(poly_left(min_y))
 poly_right = np.poly1d(np.polyfit(
 	right_line_y,
 	right_line_x,
-	deg = 1
+	deg = 1		# TypeError: expected non-empty vector for x (x = NX.asarray(x) + 0.0, x.size == 0)
 ))
 
 right_x_start = int(poly_right(max_y))
 right_x_end = int(poly_right(min_y))
 
+
+# if left_line_x is not None and left_line_y is not None:
+# 	try:
+# 		poly_left = np.poly1d(np.polyfit(
+# 			left_line_y,
+# 			left_line_x,
+# 			deg = 1
+# 		))
+# 		print(poly_left)
+# 		left_x_start = int(poly_left(max_y))
+# 		left_x_end = int(poly_left(min_y))
+# 		print(left_x_start, left_x_end)
+# 	except:
+# 		pass
+# 		left_x_start = int(poly_left(max_y))
+# 		left_x_end = int(poly_left(min_y))
+#
+# if left_line_x is None or left_line_y is None:
+# 	pass
+#
+# if right_line_x is not None and right_line_y is not None:
+# 	try:
+# 		poly_right = np.poly1d(np.polyfit(
+# 			right_line_y,
+# 			right_line_x,
+# 			deg = 1
+# 		))
+# 		print(poly_right)
+# 		right_x_start = int(poly_right(max_y))
+# 		right_x_end = int(poly_right(min_y))
+# 		print("OK")
+# 	except:
+# 		pass
+# 		print("no")
+# 		right_x_start = int(poly_right(min_y))
+# 		right_x_end = int(poly_right(max_y))
+# 		# right_x_start = 0
+# 		# right_x_end = 0
+#
+# if right_line_x is None or right_line_y is None:
+# 	pass
+
 line_image = draw_lines(
 	image,
 	[[
-		[left_x_start, max_y, left_x_end, min_y],
-		[right_x_start, max_y, right_x_end, min_y],
+		[int(left_x_start), int(max_y), int(left_x_end), min_y],
+		[int(right_x_start), int(max_y), int(right_x_end), min_y],
 	]],
-	thickness=5
+#	thickness=5
+	(0, 0, 255),
+	3,
 )
 
 plt.figure()
 plt.imshow(line_image)
-"""
-plt.show()
 
+plt.show()
--- src/lane_detection/video_lane_detection.py
+++ src/lane_detection/video_lane_detection.py
@@ -26,6 +26,72 @@ def draw_lines(img, lines, color = [255, 0, 0], thickness=3):
 	img = cv2.addWeighted(img, 0.8, line_img, 1.0, 0.0)
 	return img
 
+def pipeline(image):
+	height = image.shape[0]
+	width = image.shape[1]
+	region_of_interest_verticess = [
+		(0, height),
+		(width / 2, height / 2),
+		(width, height), 
+	]
+	ret, frame = image.read()
+#	if not ret:
+#		break
+	gray_image = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+	cannyed_image = cv2.Canny(gray_image, 100, 200)
+	cropped_image = region_of_interest(cannyed_image, np.array([region_of_interest_vertices], np.int32))
+
+	lines = cv2.HoughLinesP (cropped_image, rho = 6, theta = np.pi/60, threshold = 160, lines = np.array([]), minLineLength = 40, maxLineGap = 25 )
+
+	left_line_x = []
+	left_line_y = []
+	right_line_x = []
+	right_line_y = []
+
+	for line in lines:
+		for x1, y1, x2, y2 in line:
+			slope = (y2 - y1) / (x2 - x1)
+			if math.fabs(slope) < 0.5:
+				continue
+			if slope <= 0:
+				left_line_x.extend([x1, x2])
+				left_line_y.extend([y1, y2])
+			else:
+				right_line_x.extend([x1, x2])
+				right_line_y.extend([y1, y2])
+
+	min_y = int(image.shape[0] * (3/5))
+	max_y = int(image.shape[0])
+
+	poly_left = np.poly1d(np.polyfit(
+		left_line_y,
+		left_line_x,
+		deg=1
+	))
+
+	left_x_start = int(poly_left(max_y))
+	left_x_end = int(poly_left(min_y))
+	
+
+	poly_right = np.poly1d(np.polyfit(
+		right_line_y,
+		right_line_x,
+		deg=1
+	))
+
+	right_x_start = int(poly_right(max_y))
+	right_x_end = int(poly_right(min_y))
+
+	line_image = draw_lines(
+		image,
+		[[
+			[left_x_start, max_y, left_x_end, min_y],
+			[right_x_start, max_y, right_x_end, min_y],
+		]],
+		thickness = 5,
+	)
+
+	return line_image
 
 image = cv2.VideoCapture('solidWhiteRight.mp4')
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -40,25 +106,27 @@ region_of_interest_vertices = [(0, int(height)), (int(width)/2, int(height)/2), 
 
 
 while True:
-	ret, frame = image.read()
-	if not ret:
-		break
-	gray_image = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-	cannyed_image = cv2.Canny(gray_image, 100, 200)
-	cropped_image = region_of_interest(cannyed_image, np.array([region_of_interest_vertices], np.int32))
+#	ret, frame = image.read()
+#	if not ret:
+#		break
+#	gray_image = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+#	cannyed_image = cv2.Canny(gray_image, 100, 200)
+#	cropped_image = region_of_interest(cannyed_image, np.array([region_of_interest_vertices], np.int32))
 
-	lines = cv2.HoughLinesP (cropped_image, rho = 6, theta = np.pi/60, threshold = 160, lines = np.array([]), minLineLength = 40, maxLineGap = 25 )
+#	lines = cv2.HoughLinesP (cropped_image, rho = 6, theta = np.pi/60, threshold = 160, lines = np.array([]), minLineLength = 40, maxLineGap = 25 )
+
 	#print(lines)
 
 	#out.write(cropped_image)
 	
-	line_image = draw_lines(frame, lines)
+	#line_image = draw_lines(frame, lines)
 
-	cv2.imshow('video', frame)
-	cv2.imshow('video_gray', cropped_image)
-	cv2.imshow('video_line', line_image)
+	#cv2.imshow('video', frame)
+	#cv2.imshow('video_gray', cropped_image)
+
+	#cv2.imshow('video_line', line_image)
+	cv2.imshow('video_line', pipeline)
 	cv2.waitKey(1)
-	sleep(0.1)
 #image.release()
 #out.release()
 cv2.destroyAllWindows()
